ff13f7819ef4d830322901c5e5e8b7adf57573a3
asmondnoahloo <157396430+asmondnoahloo@users.noreply.github.com>
2024-04-29 23:48:43 +0800

    Added Login and Homepage codes

--- coinguild-app/src/App.js
+++ coinguild-app/src/App.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Router, Route, Routes} from 'react-router-dom';
 import Homepage from './pages/Homepage';
+import Login from './pages/Login';
 
 
 function App() {
@@ -12,8 +13,8 @@ function App() {
       <Route path='/' element={<Homepage/>} />
       {/* <Route path='/aboutus' element={<AboutUs/>} />
       <Route path='/products' element={<Products/>} />
-      <Route path='/workwithus' element={<WorkwUs/>} />
-      <Route path='/login' element={<Login/>} /> */}
+      <Route path='/workwithus' element={<WorkwUs/>} /> */}
+      <Route path='/login' element={<Login/>} />
     </Routes>
 
     
--- coinguild-app/src/components/navbar.jsx
+++ coinguild-app/src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/PawGuild.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
@@ -8,21 +9,21 @@ const Navbar = () => {
             <div class="d-flex justify-content-center">
                 <nav className="navbar navbar-expand-sm ">
                     <img src="/images/PawGuild.png" style={{ width: '100px' }} />
-                    <a className="navbar-brand" href="#">
+                    <Link className="navbar-brand">
                         <button type="button" className="btn btn-warning navbarBtn">About us</button>
-                    </a>
-                    <a className="navbar-brand" href="#">
+                    </Link>
+                    <Link className="navbar-brand">
                         <button type="button" className="btn btn-warning navbarBtn">Products</button>
-                    </a>
-                    <a className="navbar-brand" href="#">
+                    </Link>
+                    <Link className="navbar-brand">
                         <button type="button" className="btn btn-warning navbarBtn">Claims & Services</button>
-                    </a>
-                    <a className="navbar-brand" href="#">
+                    </Link>
+                    <Link className="navbar-brand">
                         <button type="button" className="btn btn-warning navbarBtn">Work with us</button>
-                    </a>
-                    <a className="navbar-brand" href="#">
+                    </Link>
+                    <Link to="/login" className="navbar-brand">
                         <button type="button" className="btn btn-warning navbarBtn">Sign In</button>
-                    </a>
+                    </Link>
 
                 </nav>
             </div>
